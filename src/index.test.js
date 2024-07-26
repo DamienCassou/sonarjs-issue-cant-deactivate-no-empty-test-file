82d617e475062bac4e8ee1783e34882c9e812ecd
Damien Cassou <damien@cassou.me>
2024-07-26 17:17:20 +0200

    Reproduce bug

---
+++ src/index.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable sonarjs/no-empty-test-file */
+/* eslint-disable sonarjs/arguments-order */
+function divide(dividend, divisor) {
+  return dividend / divisor;
+}
+
+function doTheThing() {
+  const dividend = 15;
+  const divisor = 5;
+
+  const result = divide(divisor, dividend);
+  return result;
+}
